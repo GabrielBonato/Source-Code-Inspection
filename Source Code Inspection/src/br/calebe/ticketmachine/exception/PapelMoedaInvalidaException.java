cda405359e02aaf768119800f25a6378199f8d69
Gabriel Bonato Carneiro <gabriel.carneiro@mackenzista.com.br>
2016-09-28 21:12:29 -0300

    Update PapelMoedaInvalidaException

--- Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
@@ -4,6 +4,5 @@ package br.calebe.ticketmachine.exception;
  *
  * @author Calebe de Paula Bianchini
  */
-public class PapelMoedaInvalidaException extends Exception {
-    
+public class PapelMoedaInvalidaException extends Exception {    
 }
